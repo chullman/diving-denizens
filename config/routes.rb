c5018c9abee2a9e3d13ee6cf4c261b8c0488c5af
chullman <christopherhullman@gmail.com>
2022-07-13 08:16:07 +1000

    custom controller for rails registrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   get '/addresses/find_address', to: 'addresses#find_address', as: 'query_for_address'
   resources :addresses
-  devise_for :users
-  
+  #devise_for :users
 
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
