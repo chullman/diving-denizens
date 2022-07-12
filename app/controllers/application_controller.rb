c0a452273dfc476b4838d0649e3b0dea3608a0d2
Christopher Hullman <christopherhullman@gmail.com>
2022-07-12 17:02:52 +1000

    sign in done with username field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+    protect_from_forgery with: :exception
+
+    # With reference to https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address (viewed 12/07/2022)
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+
+        devise_parameter_sanitizer.permit(:sign_up) do |user|
+            user.permit(:email, :username, :password, :password_confirmation)
+        end
+
+        devise_parameter_sanitizer.permit(:account_update) do |user|
+            user.permit(:email, :username, :password, :password_confirmation)
+        end
+    end
+
 end
