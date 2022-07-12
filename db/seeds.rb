bdfcf60b2904a0f34a4ecf64cb2e9f04a025a302
chullman <christopherhullman@gmail.com>
2022-07-13 08:46:36 +1000

    inital seed data for users with roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-normal_role = Role.create(name: 'normal')
-admin_role = Role.create(name: 'admin')
+normal_role = Role.create!(name: 'normal')
+admin_role = Role.create!(name: 'admin')
+
+normal_test_user = User.create!(email: 'test@test.com', username: 'normaluser123', password: 'abc123', password_confirmation: 'abc123')
+admin_test_user = User.create!(email: 'admin@test.com', username: 'adminuser123', password: 'abc123', password_confirmation: 'abc123')
+
+normal_role = Role.find_by!(name: 'normal')
+admin_role = Role.find_by!(name: 'admin')
+normal_test_user = User.find_by!(email: 'test@test.com')
+admin_test_user = User.find_by!(email: 'admin@test.com')
+
+UserRole.create(role_id: normal_role.id, user_id: normal_test_user.id)
+UserRole.create!(role_id: normal_role.id, user_id: admin_test_user.id)
+UserRole.create!(role_id: admin_role.id, user_id: admin_test_user.id)
+
