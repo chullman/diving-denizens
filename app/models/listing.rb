1112d86eaf408962c045107fa0b7b6db3b8f48c9
Christopher Hullman <christopherhullman@gmail.com>
2022-07-16 10:47:12 +1000

    added relationships for categories to listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,7 @@ class Listing < ApplicationRecord
   belongs_to :listing_fee
   has_rich_text :description
   has_many_attached :images
+
+  has_many :listing_categories
+  has_many :categories, through: :listing_categories
 end
